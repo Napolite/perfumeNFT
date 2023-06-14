d73755521afa1e42c9812e4210a49c7337046902
Constantino <Okonemmanuelmma@gmail.com>
2023-06-14 23:55:01 +0100

    extend logic, added vendor function

--- contracts/Perfume.sol
+++ contracts/Perfume.sol
@@ -11,25 +11,27 @@ contract  PerfumeNFT {
         string uri;
         uint price;
         address owner;
+        string vendorID;
         bool exists;
     }
 
     struct Vendor{
         address vendor;
         bool exists;
+        string vendorID;
     }
 
     Perfume[] perfumes;
 
     mapping ( string => Perfume) private perfume;
-    mapping (address => Vendor) private vendor;
+    mapping (address => Vendor) private vendors;
     uint public totalSupply = 0;
     address owner;
 
     uint256 PerfumesMinted = 0;
 
     modifier  onlyVendor{
-        require(vendor[msg.sender].exists == true, "Caller not a vendor");
+        require(vendors[msg.sender].exists == true, "Caller not a vendor");
         _;
     }
 
@@ -39,9 +41,8 @@ contract  PerfumeNFT {
     }
 
     function  mint(string calldata _name,string calldata _uri,uint _price) external onlyVendor{
-        string memory tStamp = Strings.toString(block.timestamp);
-        string memory id = string.concat(_name[0:5],tStamp[3:5]);
-        Perfume memory newProduct = Perfume(_name, _uri, _price, msg.sender, true);
+        string memory id = string.concat(_name[0:5],Strings.toString(block.timestamp)[3:5]);
+        Perfume memory newProduct = Perfume(_name, _uri, _price, msg.sender, vendors[msg.sender].vendorID, true);
         perfumes[totalSupply] = newProduct;
 
         perfume[id] = newProduct;
@@ -51,7 +52,8 @@ contract  PerfumeNFT {
         emit Minted(_name,msg.sender);
     }
 
-    function registerVendor() external onlyOwner{
-        Vendor memory newVendor = Vendor(address, true);
+    function registerVendor(address _vendor) external onlyOwner{
+        string memory id = string.concat(Strings.toString(address)[0:4],Strings.toString(block.timestamp)[3:5]);
+        Vendor memory newVendor = Vendor(_vendor,id, true);
     }
 }
