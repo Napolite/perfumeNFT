b9d49fdfffde30acd130562324742b948c7c1204
Constantino <Okonemmanuelmma@gmail.com>
2023-06-14 22:08:37 +0100

    extended logic to include vendor

--- contracts/Perfume.sol
+++ contracts/Perfume.sol
@@ -2,10 +2,32 @@
 pragma solidity ^0.8.9;
 
 contract  PerfumeNFT {
+
+    event Minted(string name, string uri, address owner, string tokenID);
     struct Perfume {
         string name;
-        string perfID;
         string uri;
-        string owner;
+        uint price;
+        address owner;
+        bool exists;
+    }
+
+    struct Vendor{
+        address vendor;
+        bool exists;
+    }
+
+    mapping ( string => PerfumeNFT) name;
+    mapping (address => Vendor) vendor;
+
+    uint256 PerfumesMinted = 0;
+
+    modifier  onlyVendor{
+        require(vendor[msg.sender].exists == true, "Caller not a vendor");
+        _;
+    }
+
+    function  mint() external onlyVendor{
+        
     }
 }
